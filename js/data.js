d8500bc1bc45c68ef6eeb5cd521e254bcb5f4d14
LucasKleaL <lucaskleal222@outlook.com>
2021-08-21 19:09:44 -0300

    Sollarium 0.1
    -Adicionado seção landing page.
    -Adicionado seção de apresentação do cubesat e seus sensores.

---
+++ js/data.js
@@ -0,0 +1,14 @@
+
+const subsystemsData = [
+    ["1", "COMPUTADOR DE  BORDO", "O computador embarcado, é a unidade responsável porcontrolar todos os outros subsistemas do satélite.É com ele que gerenciamos as interações entre o software e hardware do sistema. Nosso computador de bordo é composto por um ESP32embutido em uma placa feita pela PION Labs."],
+    ["2", "SISTEMA DE COMUNICAÇÃO", "O subsistema de comunicação envolve o hardware e software responsável por transmitir as informações obtidas a partir dos sensores. Ainda não definimos o hardware a ser utilizado para nossa missão, mas provavelmente utilizaremos frequências de rádio UHF para as transmissões de dados e telemetria."],
+    ["3", "SISTEMA DE ENERGIA", "O subsistema de energia é reponsável por gerenciar, gerar, e distribuir a energia para todos os demais subsistemas do satélite. No momento utilizamos uma bateria da PION que acompanha o cubesat e é carregada via USB. Para uma missão real pretendemos aumentar a capacidade energética e utilizar paineis fotovoltaicos para obtenção de energia."],
+    ["4", "SENSOR DE LUMINOSIDADE", "O sensor de luminosidade é essencial para a orientação do cubesat no espaço, com ele conseguimos obter a direção em que os raios solares estão atingindo o cubesat, podendo ser utilizado para guiar a captação de energia dos paineis solares. Além do mais, com ele é possível realizar experimentos envolvendo luz UV."],
+    ["5", "GIROSCÓPIO", "O giroscópio é o sensor responsável de coletar dados sobre a movimentação do cubesat, com ele é possível obter dados da rotação nos eixos X, Y e Z. Os dados deste sensor são essencias para a telemetria e controle de atitude."],
+    ["6", "ACELERÔMETRO", "O acelerômetro, assim como o giroscópio, capta dados sobre o movimento nos eixos X, Y e Z. Porém o acelerômetro é especializado em captar medidas de quanto o cubesat está sendo impulsionado em determinada direção."],
+    ["7", "MAGNETÔMETRO", "O magnetômetro é o sensor crucial para nossa missão principal. Com ele é possível fazer medições sobre a influência do campo magnético que está interagindo com o sensor."],
+    ["8", "SENSOR DE UMIDADE", "O sensor de umidade mede a quantidade de umidade no ar que está interagindo com o sensor. Este sensor é o mesmo que faz as medições de temperatura, um sensor DHT11."],
+    ["9", "SENSOR DE TEMPERATURA", "O sensor de temperatura mede a temperatura ambiente ao redor do sensor. Este sensor é o mesmo que faz medições de umidade, um sensor DHT11."],
+    ["10", "SENSOR DE CO2", "O sensor de CO2, mede a quantidade de dióxido de carbono que está interagindo com o sensor. O monitoramento deste gás é crucial para realizar análises da atmosfera local e monitoramento de poluição. "],
+    ["11", "SENSOR DE PRESSÃO", "O sensor de pressão mede a pressão atmósferica local, muito útil para se situar na atmosfera terreste e analisar a composição atmosférica local."]
+]
---
+++ js/index.js
@@ -0,0 +1,47 @@
+var documentHeight;
+
+$(document).ready(function() {
+
+    documentHeight = $(window).height();
+
+    function sectionHeight() {
+        $(".section").height(documentHeight);
+    }
+
+    $(sectionHeight);
+
+    $(".icon-cubesat-desc").hover(function() {
+
+        var getId = this.id;
+        console.log("hover")
+
+        for (var i = 0; i < subsystemsData.length; i++) {
+
+            if (getId === subsystemsData[i][0]) {
+                $(".h1-subsistem-title").text(subsystemsData[i][1]);
+                $(".p-subsistem-desc").empty();
+                $(".p-subsistem-desc").append(subsystemsData[i][2]);
+            }
+
+        }
+
+    })
+
+});
+
+function sectionScroll(id) {
+
+    if (id === "buttonSection01") {
+        console.log("entrou primeiro if")
+        console.log("id: "+$(".bottom-arrow-icon").attr("id"))
+        $("html, body").animate({scrollTop: documentHeight}, 500);
+    }
+    else if (id === "buttonSection02") {
+        console.log("entrou segundo if")
+        $("html, body").animate({scrollTop: documentHeight * 2}, 500);
+    }
+    else if (id === "buttonSecion03") {
+        $("html, body").animate({scrollTop: documentHeight * 3}, 500);
+    }
+
+}
